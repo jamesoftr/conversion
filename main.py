1f1992173a477c66ae6d303b39b78e4864dbc91e
shinyden <chikoritaisdope2@gmail.com>
2026-05-21 22:02:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bot = commands.Bot(
     command_prefix=["a!", "A!", "!"],
     help_command=None,
     intents=intents,
-    enable_debug_events=True,
+    enable_debug_events=False,
     case_insensitive=True,   # ← add this
 )
 
