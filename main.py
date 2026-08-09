b10639667838164973d59e6f010851213610ba05
jamesoftr <kingzenplayz@gmail.com>
2026-08-09 22:06:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ async def main():
         await bot.load_extension("cogs.leaderboard_cog")
         await bot.load_extension("cogs.calculator_cog")
         await bot.load_extension("cogs.category_cog")
+        await bot.load_extension("cogs.moderation_cog")
         await bot.load_extension("cogs.welcome_cog")   # ← NEW
         await bot.start(TOKEN)
 
